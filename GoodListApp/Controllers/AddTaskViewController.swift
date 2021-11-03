f2e105cc543ecaf33d92277509a49a6934603efb
haruto-matsushima <matsu_haruto@icloud.com>
2021-11-03 23:51:56 +0900

    add filter

--- GoodListApp/Controllers/AddTaskViewController.swift
+++ GoodListApp/Controllers/AddTaskViewController.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
+import RxSwift
 
 final class AddTaskViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var titleTextFiled: UITextField!
+    
+    let taskSubject = PublishSubject<Task>()
+    var taskSubjectObservable: Observable<Task> {
+        return taskSubject.asObservable()
     }
 
-
+    @IBAction func didTapSaveButton(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "toAdd", sender: nil)
+        guard
+            let priority = Priority(rawValue: prioritySegmentedControl.selectedSegmentIndex),
+            let title = titleTextFiled.text, !title.isEmpty else {
+                return
+            }
+        let task = Task(title: title, priority: priority)
+        taskSubject.onNext(task)
+    }
 }
--- GoodListApp/Controllers/TaskListViewController.swift
+++ GoodListApp/Controllers/TaskListViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
 
 final class TaskListViewController: UIViewController {
 
@@ -15,24 +17,57 @@ final class TaskListViewController: UIViewController {
             tableView.dataSource = self
         }
     }
-    @IBOutlet private weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var prioritySegmentedControl: UISegmentedControl! {
+        didSet {
+            prioritySegmentedControl.addTarget(self, action: #selector(didChangedPriorityValue(_:)), for: .valueChanged)
+        }
+    }
+    private let disposeBag = DisposeBag()
+    private var filterdTasks = [Task]()
+    private var tasks = BehaviorRelay<[Task]>(value: [])
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
 
+    private func filterTasks(by priority: Priority?) {
+        if priority == nil {
+            self.filterdTasks = self.tasks.value
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } else {
+            self.tasks.map { tasks in
+                return tasks.filter({$0.priority == priority! })
+            }.subscribe(onNext: { tasks in
+                self.filterdTasks = tasks
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }).disposed(by: disposeBag)
+        }
+
+    }
+
     @IBAction func unwindSegue(segue: UIStoryboardSegue) {
         guard let addVC = segue.source as? AddTaskViewController else { return }
-        print("呼ばれました")
+        addVC.taskSubjectObservable.subscribe(onNext: { [ weak self] in
+            guard let self = self else { return }
+            let priority = Priority(rawValue: self.prioritySegmentedControl.selectedSegmentIndex - 1)
+            self.tasks.accept( self.tasks.value + [$0])
+            self.filterTasks(by: priority)
+        }).disposed(by: disposeBag)
+    }
+
+    @objc private func didChangedPriorityValue(_ sender: UISegmentedControl) {
+        let priority = Priority(rawValue: sender.selectedSegmentIndex - 1)
+        filterTasks(by: priority)
     }
 
 }
 
-extension TaskListViewController: UITableViewDelegate {
-
-}
-
+extension TaskListViewController: UITableViewDelegate {}
 
 extension TaskListViewController: UITableViewDataSource {
 
@@ -40,11 +75,12 @@ extension TaskListViewController: UITableViewDataSource {
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.filterdTasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = self.filterdTasks[indexPath.row].title
         return cell
     }
 }
